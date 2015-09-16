2e43af1e28bb2ffe2df4166824deaec09e6e253d
virgilvox <moheeb.zara@citrix.com>
2015-09-16 16:39:09 -0700

    peter's changes for meshblu-arduino added along with fixes

--- command.js
+++ command.js
@@ -1,25 +1,11 @@
 'use strict';
+var config = require('./meshblu.json');
+var Connector = require('./connector');
 
-var fs = require("fs");
+var connector = new Connector(config);
 
-
-fs.stat('meshblu.json', function(err, stat) {
-  if(err == null) {
-    console.log('File exists');
-
-   var config = require('./meshblu.json');
-   var Connector = require('./connector');
-   new Connector(config);
-
-  } else if(err.code == 'ENOENT') {
-    var json = {"server":"meshblu.octoblu.com","port":80,"uuid":"uuid-here","token":"token-here"} ;
-    fs.writeFile('./meshblu.json', JSON.stringify(json));
-    setTimeout(function () {
-    var config = require('./meshblu.json');
-    var Connector = require('./connector');
-    new Connector(config);
-  }, 300);
-  } else {
-    console.log('Some other error: ', err.code);
-  }
+connector.on('error', function(error) {
+  console.error(error);
 });
+
+connector.run();
